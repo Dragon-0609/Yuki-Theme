404c53fa6b481f3b2e63baba1c354fbd3dcd6e8f
Dragon-0609 <reddragon.levelup@gmail.com>
2022-02-06 21:55:39 +0500

    Init ShadowNames

---
+++ Yuki Theme.Core/ShadowNames.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Yuki_Theme.Core
+{
+	public static class ShadowNames
+	{
+		public static string [] names => new string []
+		{
+			"Default Text", 
+			"Wallpaper", 
+			"Sticker", 
+			"Line Number", 
+			"Selection", 
+			"Vertical Ruler", 
+			"Invalid Line",
+			"Caret",
+			"Fold's Line",
+			"Fold's Rectangle",
+			"Other Marker",
+			"Number",
+			"Comment",
+			"String",
+			"Keyword",
+			"Begin, End",
+			"Special Character",
+			"Punctuation",
+			"",
+			"",
+			
+		};
+	}
+}
