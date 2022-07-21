403defa1645af4b982fab4d8cf6f9729bdcff8aa
Dragon-0609 <reddragon.levelup@gmail.com>
2022-07-21 22:57:29 +0500

    Nothing special. Try to make file drop work.

--- Yuki Theme.Core.WPF/FileExtensions.cs
+++ Yuki Theme.Core.WPF/FileExtensions.cs
@@ -3,6 +3,7 @@
 	public class FileExtensions
 	{
 		public static readonly string [] ImageExtensions = new string [] { ".jpg", ".png", ".gif" };
+		public static readonly string [] PascalExtensions = new string [] { ".pas", ".paspart_" };
 
 		public static readonly string [] ThemeExtensions = new string [] { ".yukitheme", ".yuki", ".json", ".icls", ".xshd" };
 	}
