6cbf4d2edc23211a4d3643acd71e2c6a2a5b8d50
Dragon-0609 <reddragon.levelup@gmail.com>
2022-11-16 00:11:20 +0500

    Start Adding Enchanced WPF ThemeDownloader.

---
+++ Yuki Theme.Core.WPF/Controls/ThemePreview.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Yuki_Theme.Core.WPF.Controls
+{
+	public partial class ThemePreview : UserControl
+	{
+		public ThemePreview ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ Yuki Theme.Core.WPF/Windows/ThemeDownloaderWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Yuki_Theme.Core.WPF.Windows
+{
+	public partial class ThemeDownloaderWindow : Window
+	{
+		public ThemeDownloaderWindow ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
